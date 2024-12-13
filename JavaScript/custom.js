80ee20a996b40418cebba5b1a5b25dbe2edfda61
Ahmad <mianahmadaliali123@gmail.com>
2024-12-13 08:09:16 +0500

    I corrected many mistakes

--- JavaScript/custom.js
+++ JavaScript/custom.js
@@ -1,11 +1,30 @@
 document.getElementById("Alert_Name").onclick = function() {
     alert("Mian Ahmad Ali");
-};
+    let Statement = "alert ('Mian Ahmad Ali')"
+    document.getElementById("Statement").innerHTML = Statement;
+    document.getElementById("Output").innerHTML = "";
+
+}
 
 document.getElementById("Alert_Number").onclick = function() {
     alert(123456789);
+    document.getElementById("Statement").innerHTML = "";
+    document.getElementById("Output").innerHTML = "";
+    let Statement = "alert('123456789')"
+    document.getElementById("Statement").innerHTML = Statement;
+
+
 };
 document.getElementById("Show_variable_Names").onclick = function() {
+      document.getElementById("Statement").innerHTML="";
+      document.getElementById("Output").innerHTML="";
+      let html = "<ul><li>A Variable name Can't contain any spaces.</li><li>A variable name can contain only latters,numbers,dollar signs,and underscore</li><li>Though a variable name can't be any of JavaScript's Keywords. For example,<code>userAlert</code> and <code> myVar</code> are legal.</li><li>Captal letters are fine, but be careful.Variable names are case sensitive.A ,code> rose </code> is not a <code> Rose </code>. If you assign the string 'Floribunda' to the Variable <code>roe</code>. and then ask JavaScript for the value assigned to <code>Rose</code>,you 'ill come up empty.</li></ul>"
+
+      document.getElementById("Output").innerHTML=html;
+      
+
+
+
     const fullName = "Mian Ahmad";
     let lastName = "Ali";
     let age = 21; // Fixed variable name casing
@@ -19,6 +38,10 @@ document.getElementById("Show_variable_Names").onclick = function() {
 };
 
 document.getElementById("Show-camel_case_example").onclick = function() {
+    document.getElementById("Statement").innerHTML = "";
+    document.getElementById("Output").innerHTML = "";
+    let html = "<code><ul><li>element = 'getelementbyid'</li><li>camelcase = getElementById</li><li>snakecase =  get_element_by_id </li><li>Pascalcase = GetElementById</li></ul></code>"
+    document.getElementById("Output").innerHTML = html;
     let element = "getelementbyid";
     let camelcase = "getElementById";
     let snakecase =  "get_element_by_id";
@@ -30,38 +53,65 @@ document.getElementById("Show-camel_case_example").onclick = function() {
     console.log("Pascalcase",Pascalcase );
 };
 document.getElementById("Sum2number").onclick = function() {
-   num1 = 2;
-   num2 = 3;
-   result = num1 + num2;
+    document.getElementById("Statement").innerHTML = "";
+    document.getElementById("Output").innerHTML = "";
 
-   console.log(`Sum of ${num1} + ${num2} is:` , result);
+    let num1 = 2;
+    let num2 = 3;
+    let result = num1 + num2;
 
+    document.getElementById("Output").innerHTML = "<p class='text-center'>" + result + "</p>";
 
+    let Statement = "let num1 = " + num1 + ";<br> let num2 = " + num2 + ";<br> let sum =  " + num1 + "+" + num2 + ";"
+    document.getElementById("Statement").innerHTML = Statement;
 
+    console.log(`Sum of ${num1} + ${num2} is:`, result);
 };
+
 document.getElementById("Subsctract2number").onclick = function() {
+    document.getElementById("Statement").innerHTML = "";
+    document.getElementById("Output").innerHTML = "";
     let num1 = 2; // Declare variables with `let`
     let num2 = 3;
     let result = num1 - num2; // Calculate the subtraction
 
     // Log the correct message with the result
+    
+    document.getElementById("Output").innerHTML = "<p class='text-center'>" + result + "</p>";
+
+    let Statement = "let num1 = " + num1 + ";<br> let num2 = " + num2 + ";<br> let Subtraction is  = " + num1 + "-" + num2 + ";";
+    document.getElementById("Statement").innerHTML = Statement;
    
     console.log(`Subtraction of ${num1} - ${num2} is:`, result);
 };
 
 document.getElementById("Multiply2numbers").onclick = function() {
+    document.getElementById("Statement").innerHTML = "";
+    document.getElementById("Output").innerHTML = "";
     let num1 = 10; // Declare variables with `let`
     let num2 = 30;
     let result = num1 * num2; 
+    
+    document.getElementById("Output").innerHTML = "<p class='text-center'>" + result + "</p>";
+
+    let Statement = "let num1 = " + num1 + ";<br> let num2 = " + num2 + ";<br> let Multiplication of  = " + num1 + "*" + num2 + ";";
+    document.getElementById("Statement").innerHTML = Statement;
 
     // Log the correct message with the result
    
     console.log(`Multiplication of ${num1} * ${num2} is:`, result);
 };
 document.getElementById("Division2numbers").onclick = function() {
+    document.getElementById("Statement").innerHTML = "";
+    document.getElementById("Output").innerHTML = "";
     let num1 = 10; // Declare variables with `let`
     let num2 = 30;
     let result = num1 / num2; 
+    
+    document.getElementById("Output").innerHTML = "<p class='text-center'>" + result + "</p>";
+
+    let Statement = "let num1 = " + num1 + ";<br> let num2 = " + num2 + ";<br> let Division = " + num1 + "/" + num2 + ";";
+    document.getElementById("Statement").innerHTML = Statement;
 
     // Log the correct message with the result
    
@@ -69,15 +119,28 @@ document.getElementById("Division2numbers").onclick = function() {
 };
 
 document.getElementById("CalculatesomeNumbers").onclick = function() {
+    document.getElementById("Statement").innerHTML = "";
+    document.getElementById("Output").innerHTML = "";
+
     let num1 = 10; // Declare variables with `let`
     let num2 = 30;
-    let result = num1 / num2; 
-    Calculate = result * 3
+    let num3 = 6;
+    let num4 = 4;
+
+    // Perform the calculation
+    let result = num2 / num3 * num1 + num3 ** num4 - (num2 + num1);
+
+    // Display the result in the Output element
+    document.getElementById("Output").innerHTML = "<p class='text-center'>" + result + "</p>";
+
+    // Construct and display the Statement
+    let Statement = `let Calculate = (${num2} / ${num3}) * ${num1} + ${num3} ** ${num4} - (${num2} + ${num1});`;
+    document.getElementById("Statement").innerHTML = Statement;
 
     // Log the correct message with the result
-   
-    console.log(`The Calculation of number ${result} * ${3} is:`, Calculate);
+    console.log(`The calculation of (${num2} / ${num3}) * ${num1} + ${num3} ** ${num4} - (${num2} + ${num1}) is:`, result);
 };
+
 
 
 
